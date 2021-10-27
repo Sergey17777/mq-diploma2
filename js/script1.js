72b17b4c44acd1f81d9028ce0bb034f77814d6d0
Sergey17777 <92942323+Sergey17777@users.noreply.github.com>
2021-10-27 15:49:23 +0500

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -1,10 +1,11 @@
-function closePopup()
-{
-    this.closest('.p-feedback').style.display = 'none';
-}
- 
-let elements = document.querySelectorAll('.popup-close');
- 
-for (let e of elements) {
-    e.addEventListener('click', closePopup);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const close = function() {
+        this.closest('.p-feedback').style.display = 'none';
+    }
+     
+    let elements = document.querySelectorAll('.popup-close');
+     
+    for (let e of elements) {
+        e.addEventListener('click', close);
+    }
+});
